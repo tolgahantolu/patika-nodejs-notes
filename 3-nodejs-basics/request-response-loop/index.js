83bd3a046496c01369118d210a5c451687df4ffc
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-06-26 11:37:18 +0300

    req and res loop

---
+++ 3-nodejs-basics/request-response-loop/index.js
@@ -0,0 +1,3 @@
+//! Node.js ile yazılan bir web uygulaması temelde bir request - response (istek - cevap) döngüsüdür. Bizler internet tarayıcımıza www.google.com yazmakla, aslında uzaktaki sunucuya bir request gönderiyoruz. Uzaktaki sunucu ise bize gördüğümüz Google sayfasını göstererek bir response dönüyor.
+
+//! Request HEADERS içerisinde yaptığımız istek, Response HEADERS içerisinde ise aldığımız cevap ile ilgili bilgiler taşınır. Bunlardan bazıları istek metodu, remote address, cantent-type, path vs.
