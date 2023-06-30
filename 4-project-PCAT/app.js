ec789e9c8b59d916cc6f1669bbf7e4e0bf26a368
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-06-30 11:00:10 +0300

    express and nodemon setup

--- 4-project-PCAT/app.js
+++ 4-project-PCAT/app.js
@@ -1,3 +1,17 @@
-const fn = () => {
-  console.log('function working');
-};
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  const photo = {
+    id: 1,
+    name: 'Photo Name',
+    description: 'Photo Description',
+  };
+  res.send(photo);
+});
+
+const port = 3000 || process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
