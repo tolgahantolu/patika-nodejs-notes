4ae57d081baaccd7dadc8831127a347850b3d434
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-06-30 11:49:01 +0300

    staticFiles and middleware

--- 4-project-PCAT/app.js
+++ 4-project-PCAT/app.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+//!! MY Middlewares !!
+//const myLogger = (req, res, next) => {
+//  console.log(`middleware log 1 `);
+//  next(); //! diğer middleware'e geçer, response tamamlanır.
+//};
+//const myLogger2 = (req, res, next) => {
+//  console.log(`middleware log 2 `);
+//  next(); //! diğer middleware'e geçer, response tamamlanır.
+//};
+//app.use(myLogger);
+//app.use(myLogger2);
+
+app.use(express.static('public')); //! req-res loop içindeki her şeye middleware denir, express.startic de bir middleware.
+
 app.get('/', (req, res) => {
-  const photo = {
-    id: 1,
-    name: 'Photo Name',
-    description: 'Photo Description',
-  };
-  res.send(photo);
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000 || process.env.PORT;
