ac5ec51374cb1a939d4e6ef26e0c3f712206207b
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-07-01 10:30:34 +0300

    template engine: ejs

--- 4-project-PCAT/app.js
+++ 4-project-PCAT/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-//!! MY Middlewares !!
+// template engine
+app.set('view engine', 'ejs'); //! bizim view klasörümüze bakar
+
+//!! Middlewares !!
 //const myLogger = (req, res, next) => {
 //  console.log(`middleware log 1 `);
 //  next(); //! diğer middleware'e geçer, response tamamlanır.
@@ -17,8 +21,18 @@ const app = express();
 
 app.use(express.static('public')); //! req-res loop içindeki her şeye middleware denir, express.startic de bir middleware.
 
+// routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  //  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  //  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  //  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('add');
 });
 
 const port = 3000 || process.env.PORT;
