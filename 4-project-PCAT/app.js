793dd1e12ec1aeaa28387c80674293da950478f1
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-07-05 11:40:41 +0300

    delete photo

--- 4-project-PCAT/app.js
+++ 4-project-PCAT/app.js
@@ -40,7 +40,11 @@ app.use(express.json());
 
 app.use(fileUpload());
 
-app.use(methodOverride('_method'));
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'], //! hangi methodların override edileceği...
+  })
+);
 
 //! ROUTES
 app.get('/', async (req, res) => {
@@ -102,6 +106,18 @@ app.put('/photos/:id', async (req, res) => {
   photo.save();
 
   res.redirect(`/photos/${req.params.id}`);
+});
+
+app.delete('/photos/:id', async (req, res) => {
+  const photo = await Photo.findOne({ _id: req.params.id });
+
+  let deletedImage = __dirname + '/public' + photo.image;
+
+  fs.unlinkSync(deletedImage);
+
+  await Photo.findByIdAndRemove(req.params.id);
+
+  res.redirect('/');
 });
 
 const port = 3000 || process.env.PORT;
