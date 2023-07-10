9f7b96af6c5b702996cb77165922f0b740b15572
Tolgahan Tolu <tolgahanntolu@gmail.com>
2023-07-10 17:10:42 +0300

    user login

--- 6-project-SmartEdu/controllers/authController.js
+++ 6-project-SmartEdu/controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
   try {
@@ -7,6 +8,21 @@ exports.createUser = async (req, res) => {
       status: "success",
       user,
     });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+    console.log(error.message);
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    const same = bcrypt.compare(password, user.password);
+    if (same) res.status(200).send("YOU ARE LOGGED IN");
   } catch (error) {
     res.status(400).json({
       status: "fail",
